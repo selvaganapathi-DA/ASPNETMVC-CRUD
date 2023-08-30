e1df16043a6f99a36230db55f659d8b34e5dad59
SELVAGANAPATHI R <71866457+selvaganapathi-DA@users.noreply.github.com>
2023-08-30 23:21:26 +0530

    Initial commit

---
+++ ASPNETMVC CRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ASPNETMVC_CRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+namespace ASPNETMVC_CRUD.Data
+{
+    public class MVCDemoDbContext:DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        }
+
+}
---
+++ ASPNETMVC CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNETMVC_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
